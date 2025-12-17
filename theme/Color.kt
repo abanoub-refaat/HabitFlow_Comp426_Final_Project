cd1064297a0f71dab08c0cefabf6a6a92e002594
Assem <ae3077977@gmail.com>
2025-12-17 09:50:49 +0200

    Add new files

---
+++ theme/Color.kt
@@ -0,0 +1,39 @@
+package com.example.habitflow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val PrimaryLight = Color(0xFF6B8E9E)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFD1E4ED)
+val OnPrimaryContainerLight = Color(0xFF1F3238)
+
+val SecondaryLight = Color(0xFF7FA896)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFD4F0E1)
+val OnSecondaryContainerLight = Color(0xFF1F3328)
+
+val BackgroundLight = Color(0xFFFAFBFB)
+val OnBackgroundLight = Color(0xFF1A1C1C)
+val SurfaceLight = Color(0xFFFAFBFB)
+val OnSurfaceLight = Color(0xFF1A1C1C)
+val SurfaceVariantLight = Color(0xFFE1E3E3)
+val OnSurfaceVariantLight = Color(0xFF44474A)
+
+// Dark Theme Colors
+val PrimaryDark = Color(0xFF92B8C9)
+val OnPrimaryDark = Color(0xFF0D2630)
+val PrimaryContainerDark = Color(0xFF2F4A54)
+val OnPrimaryContainerDark = Color(0xFFD1E4ED)
+
+val SecondaryDark = Color(0xFFB8D4C6)
+val OnSecondaryDark = Color(0xFF0D2618)
+val SecondaryContainerDark = Color(0xFF3A5245)
+val OnSecondaryContainerDark = Color(0xFFD4F0E1)
+
+val BackgroundDark = Color(0xFF1A1C1C)
+val OnBackgroundDark = Color(0xFFE2E2E5)
+val SurfaceDark = Color(0xFF1A1C1C)
+val OnSurfaceDark = Color(0xFFE2E2E5)
+val SurfaceVariantDark = Color(0xFF44474A)
+val OnSurfaceVariantDark = Color(0xFFC4C7CA)
